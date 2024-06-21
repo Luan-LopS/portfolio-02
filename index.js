e52f62b7195577662498e6c85357fe21de34d718
Luan <luam10@hotmail.com.br>
2024-06-21 16:54:43 -0300

    backend node.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer')
+const express = require('express')
+
+const app= express()
+
+app.use(express.urlencoded({extended:true}))
+
+const transport = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth:{
+        user: 'contatodevluan@gmail.com',
+        pass: 'notm hamz xiaa azdc'
+    }
+})
+
+app.post('/enviar', (req,res)=>{
+    const {nome, assunto, email, sugestao} = req.body
+    transport.sendMail({
+        from: '',
+        to: '',
+        subject: '',
+        html: '',
+        text: ''
+    })
+    .then(()=> console.log('Enviado com sucesso'))
+    .catch((err)=>console.log('Erro ao enviar email', err))
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
